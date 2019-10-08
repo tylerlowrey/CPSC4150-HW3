2390df6ca19eb658a3836683a04efdade4d5c40f
Blake <Blakewashburn1@gmail.com>
2019-10-08 19:13:07 -0400

    Removed unnecessary details file

--- app/src/main/java/com/tylerlowrey/one_hw3/DetailsFragment.java
+++ app/src/main/java/com/tylerlowrey/one_hw3/DetailsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,6 +13,18 @@ public class DetailsFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState)
     {
+
+        Toast loadingWeatherToast = Toast.makeText(getContext(), "Grabbing Weather Data, One Moment Please", Toast.LENGTH_SHORT);
+        loadingWeatherToast.show();
+
+        Bundle args = getArguments();
+        String cityName = args.getString("name");
+        double cityLatitude = args.getDouble("latitude");
+        double cityLongitude = args.getDouble("longitude");
+
+        //TODO: Pull weather conditions from model
+        //TODO: loadingWeatherToast.cancel()   <- check to see if this doesnt throw error
+
         return inflater.inflate(R.layout.fragment_details_land_test, container, false);
     }
 }
